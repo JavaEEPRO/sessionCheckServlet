6fb80e9da33b65e411e69fe3bdd80f6a67b80f66
JavaEEPRO <j2eengineer@gmail.com>
2017-11-22 16:31:56 +0200

    servlet completed (checks for session)

---
+++ src/main/java/si/meeting/Servlet.java
@@ -0,0 +1,26 @@
+package si.meeting;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "servlet")
+public class Servlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().append("forgive me Lord " + req.getMethod());
+
+        HttpSession session = req.getSession();
+
+        if (session.getValue("name") == null) {
+            session.putValue("name", "forgive us Lord in this session");
+            resp.getWriter().append(" (no session yet) ");
+        }else{
+            resp.getWriter().append((String)session.getValue("name"));
+        }
+    }
+}
